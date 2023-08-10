630e77c8c957c363e902c5130f3ad160e475fd8a
Jeong Wonjun <67366101+Pongchi@users.noreply.github.com>
2023-08-10 11:16:19 +0000

    learn nuxt3

---
+++ pages/products/index.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+        <p>
+            HelloWorld!!!!
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
